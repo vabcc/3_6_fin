5b5c5a5a48910af4fffbbd4466f1714672b2ca0b
vabcc <smerh-8@yandex.ru>
2021-04-12 08:50:20 +0300

    coom

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_language_transmission(browser):
+def test_autotests_start_for_different_language(browser):
     browser.get(link)
     time.sleep(11)
     add_to_basket = browser.find_element(By.CSS_SELECTOR, 'form[class="add-to-basket"]>button[type="submit"]')
